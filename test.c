00058df25e3b936b275a768ee1cdfc103dcf2a22
Olaf Faaland <faaland1@llnl.gov>
2021-12-07 14:50:49 -0800

    Add test code based on flux-job-list-id

---
+++ test.c
@@ -0,0 +1,111 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <pwd.h>
+#include <assert.h>
+#include <locale.h>
+#include <jansson.h>
+#include <argz.h>
+#include <flux/core.h>
+#include <flux/optparse.h>
+
+static const char *attrs_json_str =
+	"[\"userid\",\"state\",\"expiration\"]";
+
+int log_msg_exit (char *s)
+{
+    printf("%s\n", s);
+}
+
+int log_err_exit (char *s)
+{
+    printf("error: %s\n", s);
+}
+
+int cmd_list (flux_jobid_t id)
+{
+    int max_entries = 5;
+    flux_t *h;
+    flux_future_t *f;
+    json_t *jobs;
+    size_t index;
+    json_t *value;
+    uint32_t userid;
+    int states = 0;
+
+    if (!(h = flux_open (NULL, 0)))
+        log_err_exit ("flux_open");
+
+    /*
+    if (optparse_hasopt (p, "all-user") || optparse_hasopt (p, "all"))
+        states = FLUX_JOB_STATE_ACTIVE | FLUX_JOB_STATE_INACTIVE;
+    else if (optparse_hasopt (p, "states"))
+        states = parse_arg_states (p, "states");
+    else
+    */
+
+    states = FLUX_JOB_STATE_PENDING | FLUX_JOB_STATE_RUNNING;
+
+    /*
+        userid = getuid ();
+    */
+    userid = FLUX_USERID_UNKNOWN;
+
+    if (!(f = flux_job_list (h, max_entries, attrs_json_str, userid, states)))
+        log_err_exit ("flux_job_list");
+    if (flux_rpc_get_unpack (f, "{s:o}", "jobs", &jobs) < 0)
+        log_err_exit ("flux_job_list");
+    json_array_foreach (jobs, index, value) {
+        char *str;
+        str = json_dumps (value, 0);
+        if (!str)
+            log_msg_exit ("error parsing list response");
+        printf ("%s\n", str);
+        free (str);
+    }
+    flux_future_destroy (f);
+    flux_close (h);
+
+    return (0);
+}
+
+int main(int argc, char **argv)
+{
+	flux_jobid_t id;
+	char *flux_jobid_s;
+
+	if (argc > 2) {
+		printf("%s: too many arguments.\n", argv[0]);
+		printf("   with 0 arguments: use contents of environment variable FLUX_JOB_ID.\n");
+		printf("   with 1 argument:  use that argument as the flux job id.\n");
+		return(1);
+	} else if (argc == 2) {
+		flux_jobid_s = argv[1];
+	} else {
+		flux_jobid_s = getenv("FLUX_JOB_ID");
+		if (flux_jobid_s == NULL) {
+			printf("%s: no argument specifying Job ID and no environment variable FLUX_JOB_ID exists\n", argv[0]);
+			return(1);
+		}
+		printf("%s: using FLUX_JOB_ID %s\n", argv[0], flux_jobid_s);
+	}
+
+	if (flux_job_id_parse(flux_jobid_s, &id) < 0) {
+		printf("%s: unable to parse Job ID %s\n", argv[0], flux_jobid_s);
+		return(2);
+	}
+
+	cmd_list (id);
+
+	return(0);
+}
+
+/*
+ * vim: tabstop=4 shiftwidth=4 expandtab smartindent:
+ */
